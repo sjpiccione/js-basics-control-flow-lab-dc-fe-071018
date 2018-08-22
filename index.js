deb92f62143bd201c1b22a53e89a41ed81cf5882
Steven Piccione <sjpiccione@gmail.com>
2018-08-22 00:33:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  console.log("== scuberGreetingForFeet ==");
+    if (distance <= 400) {
+    return "This one is on me!";  
+  }
+    if ((distance > 2000) && (distance <2500)){
+    return "I will gladly take your thirty bucks.";  
+    }
+    if (distance > 2500) {
+    return "No can do.";
+    }
+}
+
+let response = scuberGreetingForFeet(199);
+
+function ternaryCheckCity(city) {
+  console.log("==  ternaryCheckCity  ==")
+  let destination = (city === "NYC") ? "Ok, sounds good." : "No go."
+  return destination;
+}
+ternaryCheckCity("NYC");
+
+
+function swtichOnCharmFromTip(){
+  console.log("==  swtichOnCharmFromTip  ==")
+  
+  switch (tip){
+    case "generous":
+      return 'Thank you so much.'
+      break;
+    case "not as generous":
+      return 'Thank you.'
+      break;
+    default:
+      return "Bye."
+  }    
+}
+swtichOnCharmFromTip("generous")
+
